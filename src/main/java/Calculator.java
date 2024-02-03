82aeb97bf5d859ee84d65828beb0b498e4b8287f
Anastasia <apavlova@yotateam.com>
2024-02-04 00:45:10 +0300

    + добавление товаров

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Calculator {
     Scanner scanner = new Scanner(System.in);
@@ -9,18 +10,18 @@ public class Calculator {
         System.out.println("Добро пожаловать в программу 'Калькулятор счета'.");
         System.out.println("Укажите в числовом виде количество гостей, на которых необходимо разделить счет:");
 
-        int numInput;
+        int numGuests;
 
         while (true) {
             String textInput = scanner.nextLine();
             if (Checking.isInt(textInput)) {
-                numInput = Integer.parseInt(textInput);
+                numGuests = Integer.parseInt(textInput);
             } else {
                 continue;
             }
-            if (numInput == 1) {
+            if (numGuests == 1) {
                 System.out.println("Для одного гостя калькулятор не нужен! Пожалуйста введите число больше 1:");
-            } else if (numInput < 1) {
+            } else if (numGuests < 1) {
                 System.out.println("Некорректный ввод! Пожалуйста введите число больше 1:");
             } else {
                 break;
@@ -56,7 +57,7 @@ public class Calculator {
             System.out.println("Товар " + product.name + " успешно добавлен.");
 
             System.out.println("Добавить еще товар? Если нет, введите 'Завершить':");
-            if (scanner.nextLine().toLowerCase().equals("завершить")) {
+            if (scanner.nextLine().equalsIgnoreCase("завершить")) {
                 break;
             }
 
@@ -70,6 +71,12 @@ public class Calculator {
         }
         System.out.println("--------------------");
         System.out.println("Сумма: " + sum);
+        double result = Math.floor(sum / numGuests);
+        String rubles = "";
+        String resultOutput = String.format(Locale.ROOT,"Каждый гость должен заплатить: %.2f %s", result, rubles);
+        System.out.println(resultOutput);
+
+
 
     }
 }
