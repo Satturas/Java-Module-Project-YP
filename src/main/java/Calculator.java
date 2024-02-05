1e33aa979b7d4e6532ebf0286b9857cc2bbe9856
Anastasia <apavlova@yotateam.com>
2024-02-05 21:56:26 +0300

    программа завершена

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -60,7 +60,7 @@ public class Calculator {
             Goods product = new Goods(goodsName, goodsPrice);
             goodsMap.put(product.name, product.price);
             sum += product.price;
-            System.out.println("Товар " + product.name + " успешно добавлен.");
+            System.out.println("Товар '" + product.name + "' успешно добавлен.");
 
             System.out.println("Добавить еще товар? Если нет, введите 'Завершить':");
             if (scanner.nextLine().equalsIgnoreCase("завершить")) {
